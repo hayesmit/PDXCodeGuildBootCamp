941d18a98eca89b1cb59a3811611db664499db0a
Mitchell Hayes <hayes_mitchell@yahoo.com>
2019-01-08 12:06:49 -0800

    emots

--- lab05-Randon_Emoticon_Generator.py
+++ lab05-Randon_Emoticon_Generator.py
@@ -1,3 +1,11 @@
 #lab05-Random_Emotico_Generator
 import random
 
+eyes = [" . . \n", " o O\n", " - -\n", " @ @\n"]
+nose = ["  U\n", "   - \n", "  ^\n"]
+mouth = [' ___\n\n', "  O\n\n", "(___)\n\n"]
+
+x = 0
+while x < 5:
+    print(random.choice(eyes)+random.choice(nose)+random.choice(mouth))
+    x += 1
