5fdcaba90af25fd7617b9d4133ead9878c1c71b8
Mitchell Hayes <hayes_mitchell@yahoo.com>
2019-01-14 11:34:00 -0800

    condence line23

--- lab18-Peaks_and_Valleys.py
+++ lab18-Peaks_and_Valleys.py
@@ -5,23 +5,27 @@ data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
 
 def drawing(data):
     everest = max(data)
+    visual = []
     while everest > 0:
         for i in range(len(data)):
+            row = []
             if data[i] >= everest:
-                print("x", end='')
-            elif data[i] < everest:
-                print(" ", end="")
+                row.append("x")
+            elif data[i] < everest and row[i-1] != " " or row[i] == row[0]:
+                row.append(" ")
+            else:
+                row.append("o")
+        visual.append(''.join(row))
         everest -= 1
         print("\n")
-
+#elif the point is beween two peaks and less than everest
 
 def peaks(data):
     peaksL = []
-    for i in range(len(data)):
-        if 0 < i < len(data)-1:
+    for i in range(1, len(data) - 1):
            # print(data[i], i, data[i-1])
-            if data[i - 1] < data[i] > data[i + 1]:
-                peaksL.append(i)
+        if data[i - 1] < data[i] > data[i + 1]:
+            peaksL.append(i)
     return peaksL
 
 def valleys(data):
