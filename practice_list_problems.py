c4d4e9da1a73886795f5c66934e145b04066c8be
Mitchell Hayes <hayes_mitchell@yahoo.com>
2019-01-14 10:09:33 -0800

    even_evens practice 3

--- practice_list_problems.py
+++ practice_list_problems.py
@@ -1,10 +1,28 @@
 #practice_list_problems.py
 import random
-players = ['Drexler', 'Jordan', 'Oden', 'James', 'Iverson']
+#players = ['Drexler', 'Jordan', 'Oden', 'James', 'Iverson']
+#
+#
+#def pick_one(option):
+#    return random.choice(option)
+#
+#
+#print(pick_one(players))
 
 
-def pick_one(option):
-    return random.choice(option)
+nums1 = [5, 6, 2]
+nums2 = [5, 5, 2]
 
 
-print(pick_one(players))
+def even_evens(nums):
+    count = 0
+    for i in nums:
+        if i % 2 == 0:
+            count += 1
+    if count % 2 == 0:
+        print(True)
+    elif count % 2 == 1:
+        print(False)
+
+
+even_evens(nums1)
